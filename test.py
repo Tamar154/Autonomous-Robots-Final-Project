bb7662de6d95514ea96d0eb017f087ba8a0ab0c8
Tamar Revazishvili <92332147+Tamar154@users.noreply.github.com>
2024-07-02 18:24:19 +0300

    test

---
+++ test.py
@@ -0,0 +1,20 @@
+import numpy as np
+import gym
+from drone_env import DroneEnv
+from dqn_agent import DQNAgent
+
+if __name__ == "__main__":
+    env = DroneEnv()
+    state_size = env.observation_space.shape[0]
+    action_size = env.action_space.n
+    agent = DQNAgent(state_size, action_size)
+    agent.load("drone-dqn.h5")
+    state = env.reset()
+    state = np.reshape(state, [1, state_size])
+    for time in range(500):
+        action = agent.act(state)
+        next_state, reward, done, _ = env.step(action)
+        next_state = np.reshape(next_state, [1, state_size])
+        state = next_state
+        if done:
+            break
